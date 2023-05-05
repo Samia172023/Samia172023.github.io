64406cedbfcd7ee7502abd88d33b571860169b07
Samia172023 <125702397+Samia172023@users.noreply.github.com>
2023-05-05 02:02:08 +0200

    Update mod4.js

--- mod4.js
+++ mod4.js
@@ -1,13 +1,11 @@
-// Get a greeting for a name
-function greeting(name) {
-    if (name.charAt(0) === "J" || name.charAt(0) === "j") {
-      return "Goodbye";
-    } else {
-      return "Hello";
-    }
-  }
-  
-  
-  
+(function(window) {
+	var speakWord = "Hello";
+	var helloSpeaker = function (name) {
+		console.log(speakWord + " " + name);
+	}
+
+	window.helloSpeaker = helloSpeaker;
+
+})(window);
  
   
